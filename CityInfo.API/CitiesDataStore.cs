3ce03ed2f0e5e3b8dbb5a7daa20e1c3db714ca08
Davor Barishic-Jaman <cro.davor@outlook.com>
2018-04-18 12:26:36 +0200

    init

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,66 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        // ensure we keep working on the same data until we restart
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            // dummy data
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Skopje",
+                    Description = "Capitol of Macedonia",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "City Park",
+                            Description = "Biggest urban park in the city"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "City Square",
+                            Description = "City centre busting with life"
+                        }
+                    }
+                },
+
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Zagreb",
+                    Description = "Capitol of Croatia",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Zagreb Cathedral",
+                            Description = "Most visited attraction in Zagreb"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Jarun Lake",
+                            Description = "Primary spot for rowing, sailing, swimming, etc"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    //[Route("apo/[controller]")] matches /api/cities for all methods/actions in this class
+    [Route("api/cities")] // cities should remain the same regardless of the name of the class
+    public class CitiesController : Controller
+    {
+        [HttpGet("/api/cities")]   // /api/cities is the routing template
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(ct => ct.Id == id);
+            if (cityToReturn == null) { return NotFound(); }
+
+            return Ok(cityToReturn);
+        }
+    }
+}
