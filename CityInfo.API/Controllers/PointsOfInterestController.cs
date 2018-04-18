ab55cedd6f6bdb44763822d02fd8ea612e21e068
Davor Barishic-Jaman <cro.davor@outlook.com>
2018-04-18 19:36:52 +0200

    add 'update', 'patch', 'delete' functionality

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -56,9 +57,86 @@ namespace CityInfo.API.Controllers
                 Name = pointOfInterest.Name,
                 Description = pointOfInterest.Description
             };
-
+                   
             return CreatedAtRoute("GetPointOfInterest", new
             { cityId, id = newPointOfInterest.Id }, newPointOfInterest);
+        }
+
+        [HttpPut("{cityId}/pointsofinterest/{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id, 
+            [FromBody] PointOfInterestUpdateDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (pointOfInterest.Description == pointOfInterest.Name)
+                ModelState.AddModelError("Description", "Fields 'name' and 'description' can't have equal values");
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(ct => ct.Id == cityId);
+            if (city == null) { return NotFound(); }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(pt => pt.Id == id);
+            if (pointOfInterestFromStore == null) { return NotFound(); }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{cityId}/pointsofinterest/{id}")]
+        public IActionResult PartiallyUpdatePointOfInterest(int cityId, int id, 
+            [FromBody] JsonPatchDocument<PointOfInterestUpdateDto> patchDocument)
+        {
+            if (patchDocument == null)
+                return BadRequest();
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(ct => ct.Id == cityId);
+            if (city == null) { return NotFound(); }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(pt => pt.Id == id);
+            if (pointOfInterestFromStore == null) { return NotFound(); }
+
+            var pointOfInterestToPatch =
+                new PointOfInterestUpdateDto()
+                {
+                    Name = pointOfInterestFromStore.Name,
+                    Description = pointOfInterestFromStore.Description
+                };
+
+            patchDocument.ApplyTo(pointOfInterestToPatch, ModelState);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (pointOfInterestToPatch.Description == pointOfInterestToPatch.Name)
+                ModelState.AddModelError("Description", "Fields 'name' and 'description' can't have equal values");
+
+            TryValidateModel(pointOfInterestToPatch);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            pointOfInterestFromStore.Name = pointOfInterestToPatch.Name;
+            pointOfInterestFromStore.Description = pointOfInterestToPatch.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{cityId}/pointsofinterest/{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(ct => ct.Id == cityId);
+            if (city == null) { return NotFound(); }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(pt => pt.Id == id);
+            if (pointOfInterestFromStore == null) { return NotFound(); }
+
+            city.PointsOfInterest.Remove(pointOfInterestFromStore);
+
+            return NoContent();
         }
     }
 }
